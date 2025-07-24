9fe0fdfa808c79412ec5c774c44b362733c0b889
Mahsa Javadi <mahsa.javady@gmail.com>
2025-07-23 21:00:27 -0400

    Implement MovieCardComponent, unit tests, and mock rendering in AppComponent

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Movie } from '../../models/movie.model';
 
 @Component({
   selector: 'app-movie-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss'],
 })
-export class MovieCardComponent {}
+export class MovieCardComponent {
+  @Input() movie!: Movie;
+  @Input() isFavorite: boolean = false;
+  @Output() movieClick = new EventEmitter<Movie>();
+  @Output() favoriteToggle = new EventEmitter<Movie>();
+
+  imageError: boolean = false;
+
+  onMovieClick(): void {
+    this.movieClick.emit(this.movie);
+  }
+
+  onFavoriteToggle(event: Event): void {
+    event.stopPropagation(); //prevent triggering movie click
+    this.favoriteToggle.emit(this.movie);
+  }
+
+  onImageError(): void {
+    this.imageError = true;
+  }
+}
